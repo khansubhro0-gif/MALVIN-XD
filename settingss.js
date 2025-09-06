ff4d262cc830031ee5d7c87a485b8cd133e8dc4c
khansubhro0-gif <khansubhro0@gmail.com>
2025-09-06 13:40:39 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~I78xWRoa#KhDqrTcvxKFsrcwLU1VIyM8EzcU7nSAp9lscC5neg0E",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
